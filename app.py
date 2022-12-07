aea8369eb6ca81f63a4802010b4744a68b2a1473
vekp <114908119+vekp@users.noreply.github.com>
2022-12-07 18:43:57 +1100

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///database.db")
-# For pythonanywhere: db = SQL("sqlite:////home/vekp/mysite/database.db")
+db = SQL("sqlite:////home/vekp/mysite/database.db")
 
 
 
